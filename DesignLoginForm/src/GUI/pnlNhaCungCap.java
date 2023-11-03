2df06cc09c5942637998bbc0d9c29b8c1178492f
LeNguyenDuyKhang <duykhang0175@gmail.com>
2023-11-03 15:57:16 +0700

    sua loi

--- DesignLoginForm/src/GUI/pnlNhaCungCap.java
+++ DesignLoginForm/src/GUI/pnlNhaCungCap.java
@@ -4,8 +4,10 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JTable;
 
 public class pnlNhaCungCap extends JPanel {
+	private JTable table;
 
 	/**
 	 * Create the panel.
@@ -17,9 +19,18 @@ public class pnlNhaCungCap extends JPanel {
 		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("this is NCC");
+		
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 35));
 		lblNewLabel.setBounds(210, 261, 313, 188);
 		add(lblNewLabel);
+		
+		table = new JTable();
+		table.setBounds(245, 477, 384, 98);
+		add(table);
+		
+		JLabel lblNewLabel_1 = new JLabel("jix");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblNewLabel_1.setBounds(175, 102, 501, 135);
+		add(lblNewLabel_1);
 	}
-
 }
